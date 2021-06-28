3b30b3a6a3bae85af458f6ca5eeaa61e0a84d6d4
adisen99 <adityasenguptasai99@gmail.com>
2021-06-29 01:12:34 +0530

    :white_check_mark: removed warning message

--- lua/codeschool/init.lua
+++ lua/codeschool/init.lua
@@ -9,12 +9,6 @@ local config = config_module.config
 local function setup(user_config)
 	if user_config then
 		config_module.apply_config(user_config)
-	elseif vim.cmd('colorscheme codeschool') then
-		vim.schedule(function()
-			vim.api.nvim_err_writeln(
-			[[codeschool.nvim: vimscript support will end soon, please change your config to lua or wrap it around lua << EOF ... EOF]]
-			)
-		end)
 	end
 	loader.load_all(config)
 end
