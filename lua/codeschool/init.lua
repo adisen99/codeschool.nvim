69e8720624c6e08d16115f600fba1b1a6be7b9ec
adisen99 <adityasenguptasai99@gmail.com>
2021-07-02 17:11:04 +0530

    removed the annoying warning messages again

--- lua/codeschool/init.lua
+++ lua/codeschool/init.lua
@@ -9,11 +9,6 @@ local config = config_module.config
 local function setup(user_config)
 	if user_config then
 		config_module.apply_config(user_config)
-	elseif vim.cmd('colorscheme codeschool') then
-		vim.schedule(function()
-			vim.api.nvim_err_write(
-			[[codeschool.nvim: Support for vimscript will end soon, please change your config to lua or wrap it around lua << EOF ... EOF]]) -- luacheck: ignore
-    end)
 	end
 	local load_plugins = loader.load_plugins(config)
 	local load_langs = loader.load_langs(config)
