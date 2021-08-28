8214d601ffce5d2e470a063715ce3c3b042ae70b
adisen99 <adityasenguptasai99@gmail.com>
2021-08-28 16:31:15 +0530

    fixing issue with nvim v0.6

--- lua/codeschool/init.lua
+++ lua/codeschool/init.lua
@@ -7,9 +7,14 @@ local config_module = require("codeschool.config")
 local config = config_module.config
 
 local function setup(user_config)
+  vim.cmd('hi clear')
+  if vim.fn.exists("syntax_on") then vim.cmd('syntax reset') end
+  vim.g.colors_name("codeschool")
+
 	if user_config then
 		config_module.apply_config(user_config)
 	end
+
 	local load_plugins = loader.load_plugins(config)
 	local load_langs = loader.load_langs(config)
 	return lush.merge({base, load_plugins, load_langs})
