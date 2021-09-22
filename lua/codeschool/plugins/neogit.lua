336096c83185db7dd5ef5e6be3aee84c01fe88e8
adisen99 <adityasenguptasai99@gmail.com>
2021-09-22 10:56:40 +0530

    fix: colors for neogit highlights

--- lua/codeschool/plugins/neogit.lua
+++ lua/codeschool/plugins/neogit.lua
@@ -1,7 +1,24 @@
--- buftabline highlights
+-- neogit highlights
 local lush = require("lush")
 local base = require('codeschool.base')
 local colors = require('codeschool.colors')
+
+local neogitaddfg = colors.bright_green
+local neogitdelfg = colors.bright_red
+
+local neogitbg1 = colors.dark2
+local neogitbg2 = colors.dark3
+local neogitbg3 = colors.dark4
+local neogitfg1 = colors.light3
+local neogitfg2 = colors.light2
+
+if vim.o.background == 'light' then
+  neogitbg1 = colors.light2
+  neogitbg2 = colors.light3
+  neogitbg3 = colors.light4
+  neogitfg1 = colors.dark2
+  neogitfg2 = colors.dark3
+end
 
 local M = {}
 
@@ -11,12 +28,11 @@ M = lush(function()
   NeogitNotificationInfo {base.CodeschoolGreen},
   NeogitNotificationWarning {base.CodeschoolYellow},
   NeogitNotificationError {fg = colors.faded_red},
-
-  NeogitDiffAddHighlight {base.CodeschoolGreenSign},
-  NeogitDiffDeleteHighlight {base.CodeschoolRedSign},
-  NeogitDiffContextHighlight {base.CodeschoolFg3},
-  NeogitHunkHeader {base.CodeschoolFg1},
-  NeogitHunkHeaderHighlight {base.CodeschoolFg1},
+  NeogitDiffAddHighlight {bg = neogitbg2, fg = neogitaddfg},
+  NeogitDiffDeleteHighlight {bg = neogitbg2, fg = neogitdelfg},
+  NeogitDiffContextHighlight {bg = neogitbg1, fg = neogitfg1},
+  NeogitHunkHeader {bg = neogitbg2, fg = neogitfg2},
+  NeogitHunkHeaderHighlight {bg = neogitbg3, fg = neogitfg2},
 	}
 end)
 
