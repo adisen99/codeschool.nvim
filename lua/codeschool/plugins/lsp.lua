ad863b3cde393d2d61a3d6c9eae1a0ae594b9872
adisen99 <adityasenguptasai99@gmail.com>
2022-07-30 01:48:18 +0530

    fix: hls_lspreference remove gui inverse

--- lua/codeschool/plugins/lsp.lua
+++ lua/codeschool/plugins/lsp.lua
@@ -34,9 +34,9 @@ M = lush(function()
     LspDiagnosticsVirtualTextWarning {base.CodeschoolWarning},
     LspDiagnosticsVirtualTextInformation {base.CodeschoolBlue},
     LspDiagnosticsVirtualTextHint {base.CodeschoolAqua},
-    LspReferenceRead {fg = base.bg0, bg = hls_lspreference, gui = styles.inverse},
-    LspReferenceText {fg = base.bg0, bg = hls_lspreference, gui = styles.inverse},
-    LspReferenceWrite {fg = base.bg0, bg = hls_lspreference, gui = styles.inverse},
+    LspReferenceRead {fg = base.bg0, bg = hls_lspreference},
+    LspReferenceText {fg = base.bg0, bg = hls_lspreference},
+    LspReferenceWrite {fg = base.bg0, bg = hls_lspreference},
 	}
 end)
 
